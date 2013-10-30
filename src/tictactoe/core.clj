ea1f90d2ef46d51ead84ff1dea2b4c8f3a84d884
Sid Raval <sidsraval@gmail.com>
2013-10-29 21:50:17 -0400

    Begins writing a few functions. Not worrying about data structures is kinda nice...

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,7 +1,29 @@
 (ns tictactoe.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn repeatv [n to-repeat]
+  (vec (repeat n to-repeat)))
+
+(defn make-board [] (vec (repeat 9 "_")))
+
+; Make this better.
+(defn winner [board player]
+  (let [winning-vector (repeatv 3 player)]
+    (or
+      (= (mapv board [0 1 2]) winning-vector)
+      (= (mapv board [3 4 5]) winning-vector)
+      (= (mapv board [6 7 8]) winning-vector)
+
+      (= (mapv board [0 3 6]) winning-vector)
+      (= (mapv board [1 4 7]) winning-vector)
+      (= (mapv board [2 5 8]) winning-vector)
+
+      (= (mapv board [0 4 8]) winning-vector)
+      (= (mapv board [2 4 6]) winning-vector))))
+
+(defn get-move [board player]
+  (let [move (read)]
+    (if (is-valid board move) (assoc board move player) false)))
+
+(defn is-valid [board position]
+  (= (get board position) "_"))
