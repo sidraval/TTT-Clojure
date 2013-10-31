26e183c8d1d85ae4f3937fbec4f881785d6748fa
Sid Raval <sidsraval@gmail.com>
2013-10-31 10:13:26 -0400

    Basically works, time to add 'AI' and clean stuff up

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,10 +1,12 @@
+; It'd be great if this could be data structure agnostic?
+; Why does it depend on vectors?
 (ns tictactoe.core
   (:gen-class))
 
 (defn repeatv [n to-repeat]
   (vec (repeat n to-repeat)))
 
-(defn make-board [] (vec (repeat 9 "_")))
+(defn make-board [] (vec (repeat 9 "__")))
 
 ; Make this better.
 (defn winner [board player]
@@ -21,9 +23,34 @@
       (= (mapv board [0 4 8]) winning-vector)
       (= (mapv board [2 4 6]) winning-vector))))
 
+(defn is-valid? [board position]
+  (= (get board position) "__"))
+
+(defn game-over? [board]
+  (or (winner board :x) (winner board :o) (not (some #{true} (mapv (partial is-valid? board) (range 0 9))))))
+
 (defn get-move [board player]
   (let [move (read)]
-    (if (is-valid board move) (assoc board move player) false)))
+    (if (is-valid? board move) (assoc board move player) false)))
 
-(defn is-valid [board position]
-  (= (get board position) "_"))
+(defn next-player [player]
+  (if (= player :x)
+    :o
+    :x))
+
+(defn print-board [board]
+  (println (mapv board [0 1 2]))
+  (println (mapv board [3 4 5]))
+  (println (mapv board [6 7 8])))
+
+; Should check winning conditions and break if not met
+(defn play [board player]
+  (print-board board)
+  (if-let [new-board (get-move board player)]
+    (if (game-over? new-board)
+      (if (winner new-board player)
+        (println "Winner!")
+        (println "Tied!"))
+      (recur new-board (next-player player)))
+    (recur board player)))
+
